f9d952ace423717b3d44e4addfcf742f7501da5d
Alfredo Paz <apaz20164920@gmail.com>
2024-11-28 21:12:48 -0600

    Adding cors configuration

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const routing = require("./app/controllers/routes");
 const {logErrors, errorHandler, boomErrorHandler} = require("./app/middlewares/error.handlers");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-routing(app)
+routing(app);
+
+const whiteList = ["http://localhost:8080", "htpps://myapp.co"];
+const options = {
+	origin: (origin, callback) => {
+		if(whiteList.includes(origin) || !origin) callback(null, true);
+		else callback(new Error("No allowed"));
+	}
+}
+
+app.use(cors(options));
 
 app.use(logErrors);
 app.use(boomErrorHandler);
